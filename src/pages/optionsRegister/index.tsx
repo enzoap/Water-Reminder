0affd93b5e93de419367d01f6c86cdee77f5fce4
Enzo <enzoap2009@hotmail.com>
2020-07-14 16:58:14 -0300

    Adicionado biblioteca de picker nativo

--- src/pages/optionsRegister/index.tsx
+++ src/pages/optionsRegister/index.tsx
@@ -1,10 +1,49 @@
-import React from "react";
-import {View, Text} from 'react-native'
+import React, {useState} from "react";
+import Constants from 'expo-constants'
+import {View, Text, StyleSheet} from 'react-native'
+import {Picker} from '@react-native-community/picker'
 
 const OptionsRegister = () => {
+  const [valuePicker, setValuePicker] = useState('1 hora')
+
     return (
-        <Text>Registrar opções</Text>
+        <View style={styles.container}>
+            <View style={styles.main}>
+                <Text style={styles.description}>Agora selecione quando você gostaria de ser lembrado.</Text>
+            </View>
+            <View style={styles.picker}>
+              <Picker selectedValue={valuePicker} onValueChange={() => {}}>
+                <Picker.Item label='1 hora' value='1 hora'/>
+                <Picker.Item label='2 horas' value='2 horas'/>
+                <Picker.Item label='3 horas' value='3 horas'/>
+              </Picker>
+            </View>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#e5f2f7',
+      padding: 32,
+      paddingTop: 20 + Constants.statusBarHeight
+    },
+
+    main: {
+      marginTop: 50,
+      justifyContent: 'center'
+    },
+    
+    description: {
+      fontFamily: 'Roboto_500Medium',
+      fontSize: 16
+    },
+
+    picker: {
+      flex: 1,
+      justifyContent: 'center'
+    }
+})
 
 export default OptionsRegister
