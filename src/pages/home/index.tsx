b8bdce67f254ac9a4eb522402e19f7fae2c927d1
Enzo <enzoap2009@hotmail.com>
2020-07-27 18:43:53 -0300

    Feito o layout da Home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,31 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native'
 import { RectButton } from "react-native-gesture-handler"
+import {Feather as Icon} from '@expo/vector-icons'
 import Constants from 'expo-constants'
 import storage from '@react-native-community/async-storage'
 
 const Home = () => {
-    async function reset(){
-        storage.clear()
-    }
+   
     return (
         <View style={styles.container}>
-            <RectButton onPress={reset}>
-                <Text>Reseta</Text>
-            </RectButton>
+            <View style={styles.topBar}>
+                <Text style={styles.title}>Water Reminder</Text>
+                <TouchableOpacity onPress={() => {}}>
+                    <Icon name="settings" size={20}/>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.main}>
+                <Text style={styles.description}>Clique no botão abaixo para registrar um copo d'água.</Text>
+                <Image style={styles.image}  source={require('../../assets/cup-of-water.png')}/>
+                <Text>200ml</Text>
+            </View>
+            <View>
+            <RectButton style={styles.button}>
+                <Text style={styles.textButton}>Registrar</Text>
+                </RectButton>
+            </View>
+           
         </View>
     )
 }
@@ -21,9 +34,52 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#e5f2f7',
-        padding: 32,
-        paddingTop: 20 + Constants.statusBarHeight
-    }   
+        padding: 30,
+        paddingTop: 25 + Constants.statusBarHeight
+    },
+
+    topBar: {
+        justifyContent: 'space-between',
+        flexDirection: 'row'
+    },
+
+    title: {
+        fontFamily: 'Ubuntu_700Bold',
+        fontSize: 28
+    },
+
+    main: {
+        marginTop: 50,
+        alignItems: 'center',
+    },
+
+    description: {
+        fontSize: 16,
+        fontFamily: 'Roboto_400Regular',
+        marginBottom: 20
+    },
+
+    button: {
+        backgroundColor: '#0191C8',
+        height: 60,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 20
+    },
+
+    textButton: {
+        fontSize: 16,
+        fontFamily: 'Roboto_500Medium'
+    },
+
+    image:{
+        width: 120,
+        height: 100,
+        resizeMode: 'center' 
+    }
+
+    
 })
 
 export default Home
